f0a6172faa8a5febafa7e70a42e6c2263247c0cc
Grant McDermott <gmcd@amazon.com>
2025-11-02 14:52:23 -0800

    add glace method

--- R/tidy.R
+++ R/tidy.R
@@ -6,7 +6,7 @@ generics::tidy
 #'
 #' @importFrom generics tidy
 #' @param x a model of class `dbreg` produced by the [dbreg()] function
-#' @param fes Should the fixed effects be tidied? Default is `FALSE`.
+#' @param fes Should the fixed effects be tidied too? Default is `FALSE`.
 #' @param ... Additional arguments to tidying method.
 #' @export
 tidy.dbreg = function(x, fes = FALSE, ...) {
@@ -17,4 +17,26 @@ tidy.dbreg = function(x, fes = FALSE, ...) {
   }
   out = data.frame(term = rownames(ct), ct, row.names = NULL)
   return(out)
+}
+
+#' glance results
+#' @export
+generics::glance
+
+#' glance `dbreg` objects
+#'
+#' @importFrom generics glance
+#' @param x a model of class `dbreg` produced by the [dbreg()] function
+#' @param ... Additional arguments to glancing method.
+#' @export
+glance.dbreg = function(x, ...) {
+  gof_vals = gof(x)
+  data.frame(
+    r.squared = gof_vals["r2"],
+    adj.r.squared = gof_vals["adj_r2"],
+    rmse = gof_vals["rmse"],
+    nobs = x$nobs_orig,
+    df.residual = x$df_residual,
+    row.names = NULL
+  )
 }
